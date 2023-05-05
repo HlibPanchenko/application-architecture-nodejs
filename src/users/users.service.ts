50b262edb2ae25874db150b4d4a7388c8ef9fbe6
HlibPanchenko <glebpanchenk7@gmail.com>
2023-05-05 17:49:42 +0300

    userRepository, create user, DB

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,21 @@
+import { UserModel } from '@prisma/client';
 import { inject, injectable } from 'inversify';
 import { IConfigService } from '../config/config.service.interface';
 import { TYPES } from '../types';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { User } from './user.entity';
+import { IUsersRepository } from './users.repository.interface';
 import { IUserService } from './users.service.interface';
 
 @injectable()
 export class UserService implements IUserService {
-	constructor(@inject(TYPES.ConfigService) private configService: IConfigService) {}
+	constructor(
+		@inject(TYPES.ConfigService) private configService: IConfigService,
+		@inject(TYPES.UsersRepository) private usersRepository: IUsersRepository,
+	) {}
 
-	async createUser({ email, name, password }: UserRegisterDto): Promise<User | null> {
+	async createUser({ email, name, password }: UserRegisterDto): Promise<UserModel | null> {
 		const newUser = new User(email, name);
 		// получаем соль из файла .env!!!
 		const salt = this.configService.get('SALT');
@@ -21,7 +26,16 @@ export class UserService implements IUserService {
 		// проверка что он есть?
 		// если есть - возвращаем null
 		// если нет - создаём
-		return null;
+		// return null;
+
+		// ищем пользователя по почте
+		const existedUser = await this.usersRepository.find(email);
+		// если такой пользователь уже зареган по такой почте
+		if (existedUser) {
+			return null;
+		}
+		// иначе создадим и вернем нового пользователя (его модель)
+		return this.usersRepository.create(newUser);
 	}
 
 	async validateUser(dto: UserLoginDto): Promise<boolean> {
